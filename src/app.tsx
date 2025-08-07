06919f17a0ab388c7779119d0099b77d9615d55f
Woojae, Park <124398+nikescar@users.noreply.github.com>
2025-08-07 11:15:24 +0900

    bump

--- src/app.tsx
+++ src/app.tsx
@@ -43,7 +43,7 @@ export default function App() {
   }
 
   return (
-    <Router root={SolidBaseRoot} basename="/ghaction-ghpage-solidbase-builder">
+    <Router root={SolidBaseRoot} base="/ghaction-ghpage-solidbase-builder">
       <ContentFilter>
         <FileRoutes />
       </ContentFilter>
